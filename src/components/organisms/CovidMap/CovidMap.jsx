f904a6e6f774a3c1a111b8a8170bfa65d79dfb9f
Lia <liafernandez2@gmail.com>
2026-05-12 22:52:11 +0200

    feat: add COVID circles to map with popup data

--- src/components/organisms/CovidMap/CovidMap.jsx
+++ src/components/organisms/CovidMap/CovidMap.jsx
@@ -1,20 +1,36 @@
 import { useEffect, useRef } from 'react'
 import L from 'leaflet'
 import 'leaflet/dist/leaflet.css'
+import axios from 'axios'
 
 const CovidMap = () => {
     const mapRef = useRef(null)
     const mapInstanceRef = useRef(null)
 
     useEffect(() => {
-        if (mapInstanceRef.current) return
+        if (mapInstanceRef.current) return  
 
-        mapInstanceRef.current = L.map(mapRef.current).setView([20, 0], 2)
+        mapInstanceRef.current = L.map(mapRef.current).setView([20, 0], 4)
 
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; OpenStreetMap contributors',
             maxZoom: 18,
         }).addTo(mapInstanceRef.current)
+
+        axios.get('https://disease.sh/v3/covid-19/countries')
+            .then(res => {
+                res.data.forEach(country => {
+                    const { lat, long } = country.countryInfo
+                    L.circle([lat, long], {
+                        color: 'red',
+                        fillColor: '#f03',
+                        fillOpacity: 0.4,
+                        radius: Math.sqrt(country.cases) * 50
+                    })
+                    .bindPopup(`<b>${country.country}</b><br>Casos: ${country.cases.toLocaleString()}`)
+                    .addTo(mapInstanceRef.current)
+                })
+            })
 
         return () => {
             mapInstanceRef.current.remove()
